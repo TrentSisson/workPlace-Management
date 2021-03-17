3ffa97621d7fba7631985366665ca1c2f91d93a4
TrentSisson <trent.c.sisson0@gmail.com>
2021-03-17 15:26:21 -0500

    rendered login/register to the dom

---
+++ src/WorkPlace.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
+import { userStorageKey } from "./components/auth/authSettings"
+import { ApplicationViews } from "./components/ApplicationViews.js"
+import { NavBar } from "./components/Nav/Nav"
+import './WorkPlace.css'
+
+export const WorkPlace = () => (
+  <>
+    <Route render={() => {
+      if (sessionStorage.getItem(userStorageKey)) {
+        return (
+          <>
+            <NavBar />
+            <ApplicationViews />
+          </>
+        )
+      } else {
+        return <Redirect to="/login" />;
+      }
+    }} />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+)
